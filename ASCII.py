8570963bb808fcf704d98a6b0dbdf0968355762b
aliibii <49719665+aliibii@users.noreply.github.com>
2021-05-18 22:21:12 +0430

    Initial commit
    add projects

---
+++ ASCII.py
@@ -0,0 +1,2 @@
+a = input("Please enter the character to print the value:")
+print("The ASCII value of '" + a + "' is", ord(a))
---
+++ app.py
@@ -0,0 +1,4 @@
+firstname = input("please enter your first name:")
+lastname = input("please enter your last name:")
+
+print ("Your name is {0} {1}!".format(firstname,lastname))
