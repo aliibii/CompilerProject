a5be2d13075dda8b148501d94aa3c904d9f1e9a9
aliibii <abakhshou@gmail.com>
2021-05-29 23:08:06 +0430

    New files

--- ProceduralPython.py
+++ ProceduralPython.py
@@ -1,12 +1,27 @@
-
-def calculate_total(sample_list):
-    total = 0
-    for x in sample_list:
-        total += x
-    return total
-
 sample_list1 = [10, 20, 30, 40]
-sample_list2 = [10, 20, 30, 40]
+total = 0
+for x in sample_list1:
+        total += x
+print(total)
 
-print(calculate_total(sample_list1))
-print(calculate_total(sample_list2))
+sample_list2 = [50, 60, 70, 80]
+total = 0
+for x in sample_list2:
+        total += x
+print(total)
+
+
+
+
+
+# def calculate_total(sample_list):
+#     total = 0
+#     for x in sample_list:
+#         total += x
+#     return total
+
+# sample_list1 = [10, 20, 30, 40]
+# sample_list2 = [50, 60, 70, 80]
+
+# print(calculate_total(sample_list1))
+# print(calculate_total(sample_list2))
---
+++ stepbystep.py
@@ -0,0 +1,14 @@
+# print("I like pizza")
+# print("It's my fave thing")
+# print("pizza pizza pizza")
+# print("Every day")
+
+def stepbystep(food):
+
+    print("I like",food)
+    print("It's my fave thing")
+    print(food,food,food)
+    print("Every day",food)
+
+food = input("What is you fave food? ")
+stepbystep(food) 
